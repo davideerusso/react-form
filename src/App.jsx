31ef877cc728420cda02a35ccac949689a12bcc4
davideerusso <rd.96.rd@gmail.com>
2025-05-26 16:45:42 +0200

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+
+useState;
 export default function App() {
-  return <></>;
+  const [newArticle, setNewArticle] = useState("");
+  const [articleList, setArticleList] = useState([
+    "Farina",
+    "Acqua",
+    "Sale",
+    "Lievito",
+  ]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newArticleLIst = [...articleList, newArticle];
+    setArticleList(newArticleLIst);
+    setNewArticle("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          value={newArticle}
+          onChange={(e) => setNewArticle(e.target.value)}
+          type="text"
+        ></input>
+        <button>invia</button>
+      </form>
+      <ul>
+        {articleList.map((article, index) => (
+          <li key={index}>{article}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
